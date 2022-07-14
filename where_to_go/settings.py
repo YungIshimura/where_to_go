9f2d4aa4614f1fb049e089ced4042706581a3621
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2022-07-14 17:22:19 +0300

    Setup media files serving

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -15,12 +15,14 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 # Quick-start development settings - unsuitable for production
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -23,4 +23,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home)
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
