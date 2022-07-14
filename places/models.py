9bd89503f909c160748875926b12fc8c47cb3f6a
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2022-07-14 17:12:00 +0300

    Add model for images

--- places/models.py
+++ places/models.py
@@ -7,6 +7,17 @@ class Event(models.Model):
     description_long = models.TextField(verbose_name='Полное описание мероприятия')
     longitude = models.FloatField(verbose_name='Долгота')
     latitude = models.FloatField(verbose_name='Широта')
-    
+
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    event = models.ForeignKey('Event', on_delete=models.SET_NULL,
+                              verbose_name='Мероприятие',
+                              related_name='images',
+                              null=True)
+    image = models.ImageField(verbose_name='Изображения')
+    
+    def __str__(self):
+        return f'{self.id} {self.event.title}'
