ae42aa42628dc39190a5f7d8edf98b5fed1b6b17
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2022-07-22 22:02:53 +0300

    Rename fields

--- places/models.py
+++ places/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Event(models.Model):
     title = models.CharField(verbose_name='Название мероприятия',max_length=250)
-    description_short = models.TextField(verbose_name='Краткое описание мероприятия')
-    description_long = models.TextField(verbose_name='Полное описание мероприятия')
+    short_description = models.TextField(verbose_name='Краткое описание мероприятия')
+    long_description = models.TextField(verbose_name='Полное описание мероприятия')
     longitude = models.FloatField(verbose_name='Долгота')
     latitude = models.FloatField(verbose_name='Широта')
 
