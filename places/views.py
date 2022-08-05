041ffe20bb34118058f88c387920d716bc1f3296
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2022-08-05 20:04:55 +0300

    Move data from template to view

--- places/views.py
+++ places/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Event
 
-# Create your views here.
+
+def home(request):
+    events = Event.objects.all()
+    features = {}
+    event_features = []
+    for event in events:
+      event_features.append({
+          "type": "Feature",
+          "geometry": {
+            "type": "Point",
+            "coordinates": [event.longitude, event.latitude]
+          },
+          "properties": {
+            "title": event.title,
+            "placeId": event.id,
+            "detailsUrl": "static/places/moscow_legends.json"
+          }
+        },
+      )
+    features['features'] = event_features
+    return render(request, 'index.html', context=features)
+    
