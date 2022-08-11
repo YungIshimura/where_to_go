f24322462f8946786f812da2fbc273da2771b33b
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2022-08-11 14:08:30 +0300

    Fix lint errors

--- places/views.py
+++ places/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 from django.urls import reverse
 from django.shortcuts import get_object_or_404, render
 from .models import Event
@@ -9,7 +9,7 @@ def home(request):
     features = {}
     event_details = []
     for event in events:
-      event_details.append({
+        event_details.append({
           "type": "Feature",
           "geometry": {
             "type": "Point",
@@ -21,21 +21,24 @@ def home(request):
             "detailsUrl": reverse('event', args=(event.id, ))
           }
         },
-      )
+        )
     features['features'] = event_details
     return render(request, 'index.html', context=features)
 
 
 def event(request, event_id):
-  event = get_object_or_404(Event, id=event_id)
-  response = {
-    "title": event.title,
-    "imgs": [image.image.url for image in event.images.all()],
-    "description_short": event.short_description,
-    "description_long": event.long_description,
-    "coordinates": {
-      "lat": event.latitude,
-      "lng": event.longitude
-    }
-    }
-  return JsonResponse(response, safe=False, json_dumps_params={'ensure_ascii': False, 'indent':2})
+    event = get_object_or_404(Event, id=event_id)
+    response = {
+      "title": event.title,
+      "imgs": [image.image.url for image in event.images.all()],
+      "description_short": event.short_description,
+      "description_long": event.long_description,
+      "coordinates": {
+        "lat": event.latitude,
+        "lng": event.longitude
+      }
+      }
+
+    return JsonResponse(response, safe=False,
+                        json_dumps_params={'ensure_ascii': False,
+                                           'indent': 2})
