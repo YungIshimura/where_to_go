75216133ddbba3be78cc3a214a1d5d2b65c0e4d9
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2022-08-22 23:08:08 +0300

    Add check flag

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -11,19 +11,18 @@ class Command(BaseCommand):
         response = requests.get(url)
         response.raise_for_status()
         event_json = response.json()
-        Event.objects.get_or_create(title=event_json['title'],
+        event = Event.objects.get_or_create(title=event_json['title'],
                                     defaults={
                                     'short_description': event_json.setdefault('description_short', ''),
                                     'long_description': event_json.setdefault('description_long', ''),
                                     'longitude':event_json['coordinates']['lng'],
                                     'latitude': event_json['coordinates']['lat']
                                     })
-        event = Event.objects.get(title=event_json['title'])
-
-        for image in event_json['imgs']:
-            response = requests.get(image)
-            response.raise_for_status()
-            image = Image.objects.create(event=event, image=ContentFile(response.content, name=f'{event.id} {event.title}' ))
+        if event[1]:
+            for image in event_json['imgs']:
+                response = requests.get(image)
+                response.raise_for_status()
+                Image.objects.create(event=event[0], image=ContentFile(response.content, name=f'{event[0].id} {event[0].title}' ))
 
     def add_arguments(self, parser):
         parser.add_argument(
