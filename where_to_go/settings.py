fcc4c9883c8174ca66d08187e7be44802bedc7b5
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2022-08-25 13:04:35 +0300

    Rename default value in STATIC_ROOT

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = env.str('STATIC_URL', default='/static/')
-STATIC_ROOT = env.str('STATIC_ROOT', default='static/')
+STATIC_ROOT = env.str('STATIC_ROOT', default='static_dist/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
